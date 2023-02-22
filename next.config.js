a2ec221bcfc76e9459736ebfd59e6739e4b89e5a
moon <moonjh9392@gmail.com>
2023-02-22 22:59:55 +0900

    build : github pages

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: '/nor2ter',
   experimental: {
     appDir: true,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
